b3698d66a30fcdecc9c81d45e4324aea7c64324c
leonardogusso <leonardogusso@gmail.com>
2025-08-31 20:58:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import { Introduction } from './components/Introduction';
 import { VideoSeparator } from './components/VideoSeparator';
 import { Calculator } from './components/Calculator';
 import { Quiz } from './components/Quiz';
+import { AskRep } from './components/AskRep'; // Importado
 import { CallToAction } from './components/CallToAction';
 import { Footer } from './components/Footer';
+import { FadeInSection } from './components/FadeInSection'; // Importado
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-sky-50 text-slate-800">
       <Header />
       <main>
-        <Introduction />
-        <VideoSeparator />
-        <Calculator />
-        <VideoSeparator />
-        <Quiz />
-        <VideoSeparator />
-        <CallToAction />
+        <FadeInSection><Introduction /></FadeInSection>
+        <FadeInSection><VideoSeparator /></FadeInSection>
+        <FadeInSection><Calculator /></FadeInSection>
+        <FadeInSection><VideoSeparator /></FadeInSection>
+        <FadeInSection><Quiz /></FadeInSection>
+        <FadeInSection><VideoSeparator /></FadeInSection>
+        <FadeInSection><AskRep /></FadeInSection> {/* Adicionado */}
+        <FadeInSection><CallToAction /></FadeInSection>
       </main>
       <Footer />
     </div>
