cb7aad63adf5f81fdc824878bc12244d1a8de152
leonardogusso <leonardogusso@gmail.com>
2025-09-01 19:44:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Introduction } from './components/Introduction';
 import { VideoSeparator } from './components/VideoSeparator';
-// A importação do Benefits foi removida
 import { Calculator } from './components/Calculator';
 import { Quiz } from './components/Quiz';
 import { AskRep } from './components/AskRep';
@@ -20,14 +19,22 @@ export default function App() {
       <main>
         <FadeInSection><Introduction /></FadeInSection>
         <FadeInSection><VideoSeparator /></FadeInSection>
-        {/* A seção Benefits foi removida daqui */}
         <FadeInSection><Calculator /></FadeInSection>
         <FadeInSection><VideoSeparator /></FadeInSection>
-        <FadeInSection><AskRep doctorName={doctorName} setDoctorName={setDoctorName} /></FadeInSection>
-        <FadeInSection><VideoSeparator /></FadeInSection>
+
+        {/* --- ORDEM CORRETA E FINAL --- */}
+
+        {/* 1. O Quiz */}
         <FadeInSection><Quiz doctorName={doctorName} /></FadeInSection>
         <FadeInSection><VideoSeparator /></FadeInSection>
+
+        {/* 2. Perguntas ao Representante (logo após o Quiz) */}
+        <FadeInSection><AskRep doctorName={doctorName} setDoctorName={setDoctorName} /></FadeInSection>
+        <FadeInSection><VideoSeparator /></FadeInSection>
+
+        {/* 3. A seção final com o download da Bula */}
         <FadeInSection><CallToAction /></FadeInSection>
+
       </main>
       <Footer />
     </div>
