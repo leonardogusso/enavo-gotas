ba4702b26146508c1263d20db9fa1061246103bb
leonardogusso <leonardogusso@gmail.com>
2025-09-01 18:51:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
-import React, { useState } from 'react'; // Adicionado useState
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Introduction } from './components/Introduction';
 import { VideoSeparator } from './components/VideoSeparator';
-import { Benefits } from './components/Benefits'; // Importa o novo componente
+// A importação do Benefits foi removida
 import { Calculator } from './components/Calculator';
 import { Quiz } from './components/Quiz';
 import { AskRep } from './components/AskRep';
@@ -12,7 +12,6 @@ import { Footer } from './components/Footer';
 import { FadeInSection } from './components/FadeInSection';
 
 export default function App() {
-  // Novo estado para guardar o nome do médico
   const [doctorName, setDoctorName] = useState('');
 
   return (
@@ -21,11 +20,9 @@ export default function App() {
       <main>
         <FadeInSection><Introduction /></FadeInSection>
         <FadeInSection><VideoSeparator /></FadeInSection>
-        <FadeInSection><Benefits /></FadeInSection> {/* Nova seção adicionada */}
-        <FadeInSection><VideoSeparator /></FadeInSection>
+        {/* A seção Benefits foi removida daqui */}
         <FadeInSection><Calculator /></FadeInSection>
         <FadeInSection><VideoSeparator /></FadeInSection>
-        {/* Passando o nome e a função para atualizar o nome para os componentes */}
         <FadeInSection><AskRep doctorName={doctorName} setDoctorName={setDoctorName} /></FadeInSection>
         <FadeInSection><VideoSeparator /></FadeInSection>
         <FadeInSection><Quiz doctorName={doctorName} /></FadeInSection>
