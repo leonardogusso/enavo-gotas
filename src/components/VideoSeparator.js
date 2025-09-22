7903a8e2ccad927843678ab948e212459e44738f
leonardogusso <leonardogusso@gmail.com>
2025-09-22 13:10:24 -0300

    Delete src/components/VideoSeparator.js

--- src/components/VideoSeparator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function VideoSeparator() {
-  return (
-    <section className="mx-auto max-w-md px-4 py-8 flex justify-center">
-      <video src="/video-gota.mp4" autoPlay muted loop playsInline className="w-24 h-24 rounded-full border-2 border-sky-300 object-cover shadow-md" />
-    </section>
-  );
-}
