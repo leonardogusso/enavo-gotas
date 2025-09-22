faaae0f152416d3ab328d113db63d57d396ecc54
leonardogusso <leonardogusso@gmail.com>
2025-09-22 13:02:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import './index.css';
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
