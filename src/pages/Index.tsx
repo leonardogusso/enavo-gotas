771e6344db170c8475fbeaec2bba6135c2ee161b
leonardogusso <leonardogusso@gmail.com>
2025-09-23 20:47:28 -0300

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Página Principal - Enavo Gotas</h1>
+      {/* Aqui virão todos os nossos componentes de seção */}
+    </div>
+  );
+}
